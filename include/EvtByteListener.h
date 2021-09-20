d80b73964991cb8b13888d7a82d606fe7e3b35f7
Matt Turner <matt@fiverings.uk>
2021-09-20 07:46:13 +0100

    x

--- include/EvtByteListener.h
+++ include/EvtByteListener.h
@@ -4,16 +4,12 @@
 #include <Arduino.h>
 #include "EvtListener.h"
 
-const byte ALWAYS = 0;
-const byte ON_CHANGE = 1;
-
 class EvtByteListener : public EvtListener
 {
 public:
     EvtByteListener(volatile byte *variable, byte targetValue, EvtAction action);
     void setupListener();
     bool isEventTriggered();
-    byte triggerMode = ON_CHANGE;
 
 private:
     volatile byte *_variable;

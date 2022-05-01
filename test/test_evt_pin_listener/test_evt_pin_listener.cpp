f8942f878f288d484f84cf9a3a4f2412426fec18
Matt Turner <matt@fiverings.uk>
2022-05-01 15:34:46 +0100

    Add some ut

--- test/test_evt_pin_listener/test_evt_pin_listener.cpp
+++ test/test_evt_pin_listener/test_evt_pin_listener.cpp
@@ -19,7 +19,7 @@ void setUp(void)
     triggered = false;
     ArduinoFakeReset();
     When(Method(ArduinoFake(), millis)).AlwaysReturn(10);
-    When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(HIGH);
+    When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(LOW);
     target.enable();
 }
 
@@ -29,17 +29,55 @@ void test_does_not_trigger_when_disabled(void)
     TEST_ASSERT_FALSE(target.isEventTriggered());
 }
 
-void test_triggers_when_enabled(void)
+void test_does_not_trigger_when_enabled_before_debounce_delay(void)
 {
-    When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(LOW);
+    When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(HIGH);
+
+    When(Method(ArduinoFake(), millis)).AlwaysReturn(20);
+    TEST_ASSERT_FALSE(target.isEventTriggered());
+
+    When(Method(ArduinoFake(), millis)).AlwaysReturn(220);
+    TEST_ASSERT_FALSE(target.isEventTriggered());
+}
+
+void test_triggers_when_enabled_after_debounce_delay(void)
+{
+    When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(HIGH);
+
+    When(Method(ArduinoFake(), millis)).AlwaysReturn(20);
+    TEST_ASSERT_FALSE(target.isEventTriggered());
+
+    When(Method(ArduinoFake(), millis)).AlwaysReturn(221);
     TEST_ASSERT_TRUE(target.isEventTriggered());
+}
+
+void test_does_not_change_when_value_changes_before_debounce_delay(void)
+{
+    When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(HIGH);
+
+    When(Method(ArduinoFake(), millis)).AlwaysReturn(20);
+    TEST_ASSERT_FALSE(target.isEventTriggered());
+
+    When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(LOW);
+    When(Method(ArduinoFake(), millis)).AlwaysReturn(221);
+    TEST_ASSERT_FALSE(target.isEventTriggered());
+}
+
+void test_must_start_opposite(void)
+{
+    When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(HIGH);
+    target.enable();
+    TEST_ASSERT_FALSE(target.isEventTriggered());
 }
 
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
     RUN_TEST(test_does_not_trigger_when_disabled);
-    RUN_TEST(test_triggers_when_enabled);
+    RUN_TEST(test_does_not_trigger_when_enabled_before_debounce_delay);
+    RUN_TEST(test_triggers_when_enabled_after_debounce_delay);
+    RUN_TEST(test_does_not_change_when_value_changes_before_debounce_delay);
+    RUN_TEST(test_must_start_opposite);
     UNITY_END();
 
     return 0;

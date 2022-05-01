a6fb784fa23e97b2b5ca943829c4fd6933a06c15
Matt Turner <matt@fiverings.uk>
2022-05-01 11:44:52 +0100

    Fix setup of unit tests

--- test/test_evt_pin_listener/test_evt_pin_listener.cpp
+++ test/test_evt_pin_listener/test_evt_pin_listener.cpp
@@ -5,41 +5,40 @@
 
 using namespace fakeit;
 
-// bool triggered = false;
-// bool trigger()
-// {
-//     triggered = true;
-//     return true;
-// }
+bool triggered = false;
+bool trigger()
+{
+    triggered = true;
+    return true;
+}
 
-// EvtPinListener target(2, 200, (EvtAction)trigger);
+EvtPinListener target(2, 200, (EvtAction)trigger);
 
 void setUp(void)
 {
-    // triggered = false;
+    triggered = false;
     ArduinoFakeReset();
-    // target.enable();
     When(Method(ArduinoFake(), millis)).AlwaysReturn(10);
-    // When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(true);
+    When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(HIGH);
+    target.enable();
 }
 
 void test_does_not_trigger_when_disabled(void)
 {
-    // target.disable();
-    // TEST_ASSERT_FALSE(target.isEventTriggered());
+    target.disable();
+    TEST_ASSERT_FALSE(target.isEventTriggered());
 }
 
 void test_triggers_when_enabled(void)
 {
-    // TEST_ASSERT_TRUE(digitalRead(2));
-    TEST_ASSERT_EQUAL(10, millis());
-    // TEST_ASSERT_TRUE(target.isEventTriggered());
+    When(Method(ArduinoFake(), digitalRead)).AlwaysReturn(LOW);
+    TEST_ASSERT_TRUE(target.isEventTriggered());
 }
 
 int main(int argc, char **argv)
 {
     UNITY_BEGIN();
-    // RUN_TEST(test_does_not_trigger_when_disabled);
+    RUN_TEST(test_does_not_trigger_when_disabled);
     RUN_TEST(test_triggers_when_enabled);
     UNITY_END();
 
